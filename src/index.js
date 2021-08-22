69b66b4ebc539efc924e51f10abf588897862b8a
sleeping-moon <alenaveremeenko2001@gmail.com>
2021-08-22 19:26:45 +0300

    fix:index

--- src/index.js
+++ src/index.js
@@ -1,36 +1,23 @@
-exports.min = function min(аrray){
-	let len=array.length;
-	let maxarr=0;
-	if(len == 0){return 0;}
-	else{
-	for(let counter=0; counter<len; counter++){
-		if(arr[counter]<maxarr){maxarr=arr[counter];}
-	  }
-		return maxarr;
-  }
-}
+exports.min = function min(array) {
+    return (
+        (array &&
+            array.reduce((acc, value) => (acc < value ? acc : value), 0)) ||
+        0
+    );
+};
 
-exports.max = function max(array){
-	let len=array.length;
-	let maxarr=0;
-	if(len == 0){return 0;}
-	else{
-	for(let counter=0; counter<len; counter++){
-		if(arr[counter]>maxarr){maxarr=arr[counter];}
-	  }
-		return maxarr;
-	}
-}
+exports.max = function max(array) {
+    return (
+        (array &&
+            array.reduce((acc, value) => (acc > value ? acc : value), 0)) ||
+        0
+    );
+};
 
-exports.avg = function avg(array){
-	let len=array.length;
-	let avgarr=0;
-	if(len == 0){return 0;}
-	else{
-	for(let counter=0; counter<len; counter++){
-		avgarr+=arr[counter];
-	  }
-		avgarr=avgarr/len;
-		return avgarr;
-	}
-}
+exports.avg = function avg(array) {
+    return (
+        (array &&
+            array.reduce((acc, value) => (acc += value), 0) / array.length) ||
+        0
+    );
+};
